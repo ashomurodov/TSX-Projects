1ea3c35a797f66d8ffe815bec9437237c231ac3d
Ayyub <ayyuubshomurodov@gmail.com>
2023-06-21 19:11:29 +0500

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./App.css";
-import { TicTacToe } from "./components/tictactoe";
 
 export default class App extends React.Component {
   render(): React.ReactNode {
-    return <TicTacToe />;
+    return <h1>Hello</h1>;
   }
 }
